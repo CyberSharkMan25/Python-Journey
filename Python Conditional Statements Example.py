c72d92977e6798f1ff4fef3434d025e34fb997ae
CyberSharkMan25 <mclark211@su.edu>
2024-10-02 00:05:37 -0400

    Add files via upload
    This program is a simple example of how to structure conditional statements (if,elif,else) with an integer value entered by the user.

---
+++ Python Conditional Statements Example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+x = int(input("Enter a number: "))  # lets prompt the user to enter a value
+
+if (x>1): # checks if the value is greater than one
+    print("Your value is greater than one.") # prints that the value is greater than one
+
+elif (x == 1): # checks if the value is equal to one
+    print("Your value is equal to one.") # prints the value is equal to one
+    
+else: # checks if the value is less than 1
+    print("Your value is less than one.") # prints that the value is less than one
+
+
+# In[ ]:
+
+
+
+
