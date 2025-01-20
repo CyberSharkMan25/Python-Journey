d85ede51b2efab1848caea8728b481e5bb2c208e
CyberSharkMan25 <mclark211@su.edu>
2025-01-20 01:59:31 -0500

    Update Rock Paper Scissors .py

--- Rock Paper Scissors .py
+++ Rock Paper Scissors .py
@@ -4,22 +4,22 @@
 # In[31]:
 
 
-import random
+import random #imports the random library
 
-options = ["ROCK", "PAPER", "SCISSORS"]
+options = ["ROCK", "PAPER", "SCISSORS"] #creates a list of the three options: Rocks, Paper, and Scissors
 
-user_score = 0
+user_score = 0 #sets user score
 
-computer_score = 0
+computer_score = 0 #sets computer score
 
-A = str(input("Do you want to play? "))
+A = str(input("Do you want to play? ")) #prompts the user if they want to play the game
 
-question = A.upper()
+question = A.upper() #converts all user input to uppercase to prevent case sensitivity issues
 
-while question == "YES":
+while question == "YES": #a while loop checks to see if the user wants to play and begins the game if they say "yes"
     
     computer = random.choice(options)
-    print(computer)
+    print(computer) # shows the random choice for the computer. Note that this is used to check if the score output using the conditional statements is correct
     
     B = str(input("Enter rock, paper, or scissors"))
     user = B.upper()
@@ -63,11 +63,11 @@ while question == "YES":
         print("input error. Please try again.\n")
     
         
-    print("The user's score is", user_score)
+    print("The user's score is", user_score) #prints the user's final score
     
-    print("The computer's score is", computer_score)
+    print("The computer's score is", computer_score) #prints the computer's final score
     
-    A = str(input("Do you want to play? "))
+    A = str(input("Do you want to play? ")) #asks the user if they want to play the game again
 
     question = A.upper()
 
